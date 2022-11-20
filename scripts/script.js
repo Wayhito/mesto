4c8ef87220a2c2cabb9cd5f8099e2dfdc57cdb03
Wayhito <moskariko@mail.ru>
2022-11-20 20:10:32 +0200

    Fixes: js+idk

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,31 @@
-let edit = document.querySelector('.profile__editbutton');
+let edit = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.popup');
 let cross = document.querySelector('.popup__cross');
-let submit = document.querySelector('.popup__submit');
+let nameInput = document.getElementsByClassName('popup__input_type_name')[0];
+let jobInput = document.getElementsByClassName('popup__input_type_job')[0];
+let name = document.getElementsByClassName('profile__name')[0];
+let job = document.getElementsByClassName('profile__job')[0];
 
 edit.addEventListener("click", openPopup);
 cross.addEventListener("click", closePopup);
-submit.addEventListener("click", save);
+formElement.addEventListener('submit', formSubmitHandler); 
 
 function openPopup() { 
     formElement.classList.add('popup_opened');
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
 }
 
 function closePopup() {
     formElement.classList.remove('popup_opened');
 }
 
-function save() {
-    let nameInput = document.querySelector('.popup__name').value;
-    let jobInput = document.querySelector('.popup__job').value;
-    let name = document.querySelector('.profile__name');
-    let job = document.querySelector('.profile__job');
- 
-if (nameInput !="")
-    name.textContent = nameInput;
-
-if (jobInput !="")
-    job.textContent = jobInput;
-
-closePopup();
-}
-
-formElement.addEventListener('submit', formSubmitHandler); 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
+
+    name.textContent = nameInput.value;
+
+    job.textContent = jobInput.value;
+
+closePopup();
 }
