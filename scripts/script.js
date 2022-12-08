5e62e50ac6045af30663761c49dc2d5bf5fd0f9d
Wayhito <moskariko@mail.ru>
2022-12-08 20:18:43 +0200

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
-let edit = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.popup');
-let cross = document.querySelector('.popup__cross');
-let nameInput = document.getElementsByClassName('popup__input_type_name')[0];
-let jobInput = document.getElementsByClassName('popup__input_type_job')[0];
-let name = document.getElementsByClassName('profile__name')[0];
-let job = document.getElementsByClassName('profile__job')[0];
-let add = document.querySelector('.profile__add-button');
-let addFormElement = document.querySelector('.add-popup');
-let addCross = document.querySelector('.add-popup__cross-button');
-let AddNameInput = document.getElementsByClassName('add-popup__input_type_name')[0];
-let AddLinkInput = document.getElementsByClassName('add-popup__input_type_link')[0];
-let Cards = document.querySelector('.elements');
-let imgPopup = document.querySelector('.img-popup');
-let popupImg = document.querySelector('.img-popup__image');
-let popupText = document.querySelector('.img-popup__text');
-let imgPopupCross = document.querySelector('.img-popup__cross-button');
+const editButton = document.querySelector('.profile__edit-button');
+const profilePopup = document.querySelector('.profile-popup');
+const profileCross = document.querySelector('.profile-popup__cross');
+const profileNameInput = document.getElementsByClassName('profile-popup__input_type_name')[0];
+const profileJobInput = document.getElementsByClassName('profile-popup__input_type_job')[0];
+const name = document.getElementsByClassName('profile__name')[0];
+const job = document.getElementsByClassName('profile__job')[0];
+const addButton = document.querySelector('.profile__add-button');
+const cardPopup = document.querySelector('.add-popup');
+const cardCross = document.querySelector('.add-popup__cross-button');
+const cardNameInput = document.getElementsByClassName('add-popup__input_type_name')[0];
+const cardLinkInput = document.getElementsByClassName('add-popup__input_type_link')[0];
+const cardField = document.querySelector('.elements');
+const imgPopup = document.querySelector('.img-popup');
+const popupImg = document.querySelector('.img-popup__image');
+const popupText = document.querySelector('.img-popup__text');
+const imgPopupCross = document.querySelector('.img-popup__cross-button');
 
-let cardCollect = new Array();
+let cards = new Array();
 const initialCards = [
     {
       name: 'Архыз',
@@ -45,29 +45,29 @@ const initialCards = [
   ]; 
 
 for (i = 0; i < initialCards.length; i++) {
-  cardCollect.push(initialCards[i]);
+  cards.push(initialCards[i]);
 }
 
-    for (i = 0; i < cardCollect.length; i++) {
+    for (i = 0; i < cards.length; i++) {
     let Card = `
 <div class="element">
     <div class="element__image-field">
-     <img class="element__image" src="${cardCollect[i].link}" alt="Картинка галереи">
+     <img class="element__image" src="${cards[i].link}" alt="Картинка галереи">
      <button class="element__remove"></button>
     </div>
      <div class="element__panel">
-        <h2 class="element__name">${cardCollect[i].name}</h2>
+        <h2 class="element__name">${cards[i].name}</h2>
         <button class="element__like-button">
         <img class="element__like" src="./images/like.svg" alt="лайк">
         </button>
     </div> 
 </div>`;
-Cards.insertAdjacentHTML('afterbegin', Card);
+cardField.insertAdjacentHTML('afterbegin', Card);
 }
 
 document.querySelectorAll('.element__like').forEach(item => {
     item.addEventListener('click', event => {
-      blackLike(item);
+      switchLikeColor(item);
     })
   })
 
@@ -83,59 +83,60 @@ document.querySelectorAll('.element__image').forEach(item => {
   })
 })
 
-edit.addEventListener("click", openPopup);
-cross.addEventListener("click", closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
-add.addEventListener("click", openAddPopup);
-addCross.addEventListener("click", closeAddPopup);
-addFormElement.addEventListener('submit', addCard);
+editButton.addEventListener("click", openProfilePopup);
+profileCross.addEventListener("click", closeProfilePopup);
+profilePopup.addEventListener('submit', profilePopupSubmit);
+addButton.addEventListener("click", openCardPopup);
+cardCross.addEventListener("click", closeCardPopup);
+cardPopup.addEventListener('submit', addCard);
 imgPopupCross.addEventListener("click", imgPopupClose);
 
-function openPopup() { 
-    formElement.classList.add('popup_opened');
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
+function openProfilePopup() { 
+    profilePopup.classList.add('profile-popup_opened');
+    profileNameInput.value = name.textContent;
+    profileJobInput.value = job.textContent;
 }
 
-function openAddPopup() {
-    addFormElement.classList.add('add-popup_opened');
+function openCardPopup() {
+    cardPopup.classList.add('add-popup_opened');
 }
 
-function closePopup() {
-    formElement.classList.remove('popup_opened');
+function closeProfilePopup() {
+    profilePopup.classList.remove('profile-popup_opened');
 }
 
-function closeAddPopup() {
-    addFormElement.classList.remove('add-popup_opened');
+function closeCardPopup() {
+    cardPopup.classList.remove('add-popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function profilePopupSubmit (evt) {
     evt.preventDefault(); 
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
-    closePopup();
+    name.textContent = profileNameInput.value;
+    job.textContent = profileJobInput.value;
+    closeProfilePopup();
 }
 
 function addCard(evt) {
     evt.preventDefault();
+    let cardForm = document.getElementById('#cardForm');
     let Card = `
     <div class="element">
     <div class="element__image-field">
-     <img class="element__image" src="${AddLinkInput.value}" alt="Картинка галереи">
+     <img class="element__image" src="${cardLinkInput.value}" alt="Картинка галереи">
      <button class="element__remove"></button>
     </div>
      <div class="element__panel">
-        <h2 class="element__name">${AddNameInput.value}</h2>
+        <h2 class="element__name">${cardNameInput.value}</h2>
         <button class="element__like-button">
         <img class="element__like" src="./images/like.svg" alt="лайк">
         </button>
     </div> 
 </div>`;
-    Cards.insertAdjacentHTML('afterbegin', Card);
-    cardCollect.push(Card);
+    cardField.insertAdjacentHTML('afterbegin', Card);
+    cards.push(Card);
     let newLike = document.querySelector('.element__like');
     newLike.addEventListener('click', event => {
-      blackLike(newLike);
+      switchLikeColor(newLike);
     })
     let newRemove = document.querySelector('.element__remove');
     newRemove.addEventListener('click', event => {
@@ -145,9 +146,13 @@ function addCard(evt) {
     openImg.addEventListener('click', event => {
       openImgPopup(openImg);
     })
+    cardLinkInput.value = "";
+    cardNameInput.value = "";
+    
+    closeCardPopup();
   }
   
-function blackLike(clicked) {
+function switchLikeColor(clicked) {
     clicked.classList.toggle('element__like_active');
 }
 
